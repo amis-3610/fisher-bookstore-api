4d70424e88922aade4b771e65cdbb139e8cff9c4
zhouyufei1 <zhou.2153@osu.edu>
2020-03-24 18:21:01 -0400

    a

--- Startup.cs
+++ Startup.cs
@@ -30,6 +30,7 @@ namespace Fisher.Bookstore
             services.AddCors();
             services.AddSingleton<IBooksRepository, TestBooksRepository>();
             services.AddSingleton<IBooksRepository, TestBooksRepository>();
+            services.AddSingleton<IBooksRepository,TestBooksRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
