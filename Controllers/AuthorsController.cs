7671702667863115ab7680da1e7c42a97bd64b53
Justin Nath <nath.36@osu.edu>
2020-04-08 11:44:54 -0400

    commit

--- Controllers/AuthorsController.cs
+++ Controllers/AuthorsController.cs
@@ -1,8 +1,63 @@
+using Fisher.Bookstore.Models;
+using Fisher.Bookstore.Services;
+using Microsoft.AspNetCore.Mvc;
 namespace Fisher.Bookstore.Controllers
 {
 
-    public class AuthorsController
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthorsController : ControllerBase
     {
+        private IAuthorsRepository authorsRepository;
+        public AuthorsController(IAuthorsRepository repository)
+        {
+            authorsRepository = repository;
+        }
 
+        [HttpGet("{authorID}")]
+        public IActionResult Get(int authorID)
+        {
+             if (!authorsRepository.AuthorExists(authorID))
+            {
+                return NotFound();
+            }
+            return Ok(authorsRepository.GetAuthor(authorID));
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody]Author author)
+        {
+            var authorId = authorsRepository.AddAuthor(author);
+            return Created($"https://localhost:5001/api/authors/{authorId}", author);
+        }
+
+        [HttpPut("{authorId}")]
+        public IActionResult Put(int authorId, [FromBody] Author author)
+        {
+            if (authorId != author.Id)
+            {
+                return BadRequest();
+            }
+
+            if (!authorsRepository.AuthorExists(authorId))
+            {
+                return NotFound();
+            }
+
+            authorsRepository.UpdateAuthor(author);
+            return Ok(author);
+        }
+
+        [HttpDelete("{authorId}")]
+        public IActionResult Delete(int authorId)
+        {
+            if (!authorsRepository.AuthorExists(authorId))
+            {
+                return NotFound();
+            }
+
+            authorsRepository.DeleteAuthor(authorId);
+            return Ok();
+        }
     }
 }
--- Startup.cs
+++ Startup.cs
@@ -23,10 +23,7 @@ namespace Fisher.Bookstore
         {
             services.AddControllers();
             services.AddCors();
-            services.AddDbContext<BookstoreContext>(options =>
-                options.UseNpgsql(Configuration.GetConnectionString("BookstoreContext"))
-            );
-            services.AddScoped<IBooksRepository, BooksRepository>();
+            services.AddSingleton<IAuthorsRepository, TestAuthorsRepository>();
             services.AddSingleton<IBooksRepository, TestBooksRepository>();
         }
 
