e92e59eb8f0863bd4368b8ff5bb55530a8d8fd36
alyelazab <59670782+alyelazab@users.noreply.github.com>
2020-04-22 23:26:40 -0400

    Lab 8 done (#3)

---
+++ Models/BookAuthor.cs
@@ -0,0 +1,11 @@
+using System.Collections.Generic;
+
+namespace Fisher.Bookstore.Models
+{
+    public class BookAuthor{
+        public int BookId { get; set; }
+        public Book Book { get; set; }
+        public int AuthorId { get; set; }
+        public Author Author { get; set; }
+    }
+}
