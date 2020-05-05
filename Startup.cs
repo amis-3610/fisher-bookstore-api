28d957dabcb5a06988adaf00a4878176669a4c63
KristynBurnham <kristynburnham@hotmail.com>
2020-05-05 14:52:32 -0400

    lab 8

--- Startup.cs
+++ Startup.cs
@@ -1,11 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Fisher.Bookstore.Data;
-using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 using Fisher.Bookstore.Services;
+using Microsoft.EntityFrameworkCore;
+using Fisher.Bookstore.Data;
 
 namespace Fisher.Bookstore
 {
@@ -15,9 +22,7 @@ namespace Fisher.Bookstore
         {
             Configuration = configuration;
         }
-
         public IConfiguration Configuration { get; }
-
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
@@ -29,7 +34,6 @@ namespace Fisher.Bookstore
             services.AddScoped<IBooksRepository, BooksRepository>();
             services.AddScoped<IAuthorsRepository, AuthorsRepository>();
         }
-
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
@@ -37,17 +41,12 @@ namespace Fisher.Bookstore
             {
                 app.UseDeveloperExceptionPage();
             }
-
             app.UseHttpsRedirection();
-
             app.UseRouting();
-
             app.UseCors(b => b.AllowAnyOrigin()
                 .AllowAnyHeader()
                 .AllowAnyMethod());
-
             app.UseAuthorization();
-
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
